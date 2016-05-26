b3361672c5da73a593b1a81b9f5eed955f3dfcc8
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2016-05-26 08:48:45 +0200

    added start shell script

---
+++ grab-start.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+export GRAB_USER=jondoe
+export GRAB_PATH=/tmp/
+export GRAB_EXPOSURE=800
+export GRAB_HEIGHT=32
+export GRAB_PID=/var/run/grab.pid
+export PYLON_ROOT=/opt/pylon5
+export GENICAM_ROOT_V2_3=$PYLON_ROOT/genicam
+export GENICAM_CACHE_v2_3=${HOME}/genicam_xml_cache
+gclibdir=$GENICAM_ROOT_V2_3/bin/Linux64_x64
+pylonlibdir=$PYLON_ROOT/lib64
+export LD_LIBRARY_PATH=$pylonlibdir:$gclibdir:$gclibdir/GenApi/Generic:$LD_LIBRARY_PATH
+/opt/grab/grab > /var/log/grab.log 2>&1
